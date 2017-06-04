f927108b8a43470b43001063ea37eaeb2c189ffb
belka <belka@BELKA-PC>
2017-06-04 14:24:45 +0400

    мой первый commit

--- MadHype/Game.h
+++ MadHype/Game.h
@@ -48,11 +48,17 @@ namespace MadHype {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Game";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// Game
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(494, 368);
+			this->Name = L"Game";
+			this->Text = L"Game";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
